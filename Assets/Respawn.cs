23c79b85e23d68865fdafc3fad4e972811f1f70d
Mukund Narayan <mukund.narayan@edu.futuregames.nu>
2023-12-04 21:14:43 +0100

    respawn and enemy
    respawn logic and enemy collision and tags are done

---
+++ Assets/Respawn.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    
+    private Vector3 _lastRespawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Respawn"))
+        {
+            _lastRespawn = other.transform.position;
+        }
+        
+        if (other.CompareTag("Enemy"))
+        {
+            CharacterController cc = GetComponent<CharacterController>();
+
+            cc.enabled = false;
+            print(transform.position);
+            print("collided with enemy");
+            transform.position = _lastRespawn;
+            print(transform.position);
+            cc.enabled = true;
+
+        }
+    }
+    
+}
